2441d05f36a338f9426890dcc9a43c680611b86d
Vruddhi <vruddhishah173@gmail.com>
2025-03-03 15:05:28 +0530

    cart

---
+++ ml/cart.py
@@ -0,0 +1,103 @@
+import numpy as np
+
+class DecisionTree:
+    def __init__(self, min_samples_split=2, max_depth=2):
+        self.min_samples_split = min_samples_split
+        self.max_depth = max_depth
+        self.tree = None
+
+    def fit(self, X, y):
+        self.tree = self._grow_tree(X, y)
+
+    def predict(self, X):
+        return np.array([self._predict(inputs) for inputs in X])
+
+    def _grow_tree(self, X, y, depth=0):
+        n_samples, n_features = X.shape
+        n_labels = len(np.unique(y))
+
+        if (depth >= self.max_depth
+                or n_labels == 1
+                or n_samples < self.min_samples_split):
+            leaf_value = self._most_common_label(y)
+            return Node(value=leaf_value)
+
+        feat_idxs = np.random.choice(n_features, n_features, replace=False)
+
+        best_feat, best_thresh = self._best_criteria(X, y, feat_idxs)
+        left_idxs, right_idxs = self._split(X[:, best_feat], best_thresh)
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
+        return Node(best_feat, best_thresh, left, right)
+
+    def _best_criteria(self, X, y, feat_idxs):
+        best_gain = -1
+        split_idx, split_thresh = None, None
+        for feat_idx in feat_idxs:
+            X_column = X[:, feat_idx]
+            thresholds = np.unique(X_column)
+            for threshold in thresholds:
+                gain = self._information_gain(y, X_column, threshold)
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+        return split_idx, split_thresh
+
+    def _information_gain(self, y, X_column, split_thresh):
+        parent_entropy = self._entropy(y)
+
+        left_idxs, right_idxs = self._split(X_column, split_thresh)
+
+        if len(left_idxs) == 0 or len(right_idxs) == 0:
+            return 0
+
+        n = len(y)
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
+
+        ig = parent_entropy - child_entropy
+        return ig
+
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column <= split_thresh).flatten()
+        right_idxs = np.argwhere(X_column > split_thresh).flatten()
+        return left_idxs, right_idxs
+
+    def _entropy(self, y):
+        hist = np.bincount(y)
+        ps = hist / len(y)
+        return -np.sum([p * np.log2(p) for p in ps if p > 0])
+
+    def _most_common_label(self, y):
+        hist = np.bincount(y)
+        return np.argmax(hist)
+
+class Node:
+    def __init__(self, feature=None, threshold=None, left=None, right=None, value=None):
+        self.feature = feature
+        self.threshold = threshold
+        self.left = left
+        self.right = right
+        self.value = value
+
+    def is_leaf_node(self):
+        return self.value is not None
+
+if __name__ == "__main__":
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split
+
+    data = datasets.load_iris()
+    X = data.data
+    y = data.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+    clf = DecisionTree(max_depth=10)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+
+    accuracy = np.sum(y_pred == y_test) / len(y_test)
+    print("Accuracy:", accuracy)
